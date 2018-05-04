bd5135f0620a065e289a50a61292b78454e7534b
TsT <tst2005@gmail.com>
2018-05-04 13:52:36 +0200

    poc1

---
+++ poc1.sh
@@ -0,0 +1,14 @@
+load() { . ./pocfunc/$1; }
+
+#load cats ; cat1 | { load greps ; grep1 | grep2 | grep3; } | { load cuts ; cut1 | { load sorts ; sort1  | cut2; }; }
+
+load cats; cat1 | {
+	load greps; grep1 | grep2 | grep3 | {
+		load cuts; cut1 | {
+			load sorts; sort1 | {
+				load cuts; cut2;
+			};
+		};
+	};
+}
+
